bf0d1cede33c9bc5b091d4b320206513da94b745
Syed Abdullah Aziz <sabdullahaziz@hotmail.com>
2018-08-10 17:42:24 +0500

    Taking first step as "setup a Spring Boot project & define Spring MVC, Hibernate and Log4J as dependencies" Please refer to POM file and check configuration thank u.

---
+++ src/main/java/tk/codelearning/avicenaclinicalapp/AvicenaclinicalappApplication.java
@@ -0,0 +1,12 @@
+package tk.codelearning.avicenaclinicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvicenaclinicalappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvicenaclinicalappApplication.class, args);
+    }
+}
